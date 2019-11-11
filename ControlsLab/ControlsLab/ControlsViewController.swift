fed023b9fe3e014ed3db542f749d7d1ffa82f6e6
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-11 17:34:29 -0500

    almost done

---
+++ ControlsLab/ControlsLab/ControlsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ControlsViewController.swift
+//  ControlsLab
+//
+//  Created by World Domination a Brand on 11/11/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import Foundation
--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -1,10 +1,77 @@
 import UIKit
 
-class ViewController: UIViewController {
+/*
+ What do I need :
+ 
+ - how to increment stepper/ value of card
+ *link to view controller
+ - how to change the suit color
+ - input the suit color into image view
+ 
+ 
+ */
 
+class ViewController: UIViewController {
+    
+    // MARK: Outlets and Variables
+    
+    // allows for the changew of the suit
+    @IBOutlet weak var suitSegmentSelector: UISegmentedControl!
+    
+    // allows for the changew of the value
+    @IBOutlet weak var valueIncrementorStepper: UIStepper!
+    
+    // allows for the changew of the image based on the suit and the value
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // variables:
+    
+    var currentSuitSegement: Int = 1 {
+        //to show the suit colot
+        didSet {
+            //so each time the segment changes the image will change as well.
+            switch suitSegmentSelector.selectedSegmentIndex{
+            case 1 :
+                UIImage(systemName: "heart")
+                
+            // to add in the funky image type image literal
+            case 2 :
+                UIImage(systemName: "diamond")
+            case 3 :
+                UIImage(systemName: "club")
+            case 4 :
+                UIImage(systemName: "spade")
+                
+            default :
+                UIImage(systemName: "square")
+            }
+            
+        }
+        
+        /*
+         11 = j
+         12 = q
+         13 = k
+         14 = A
+         */
+        
+        var valueIncrementor: Int = 1 {
+        didSet {
+        // will increment the image on uiimagew.
+        }
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    
 }
+
+
 
